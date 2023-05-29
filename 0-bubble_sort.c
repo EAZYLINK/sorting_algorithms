1d8dbffbcde026fdfebd03ebfb334b06abab8a95
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-05-29 03:28:47 +0100

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,27 +6,23 @@
  */
 
 void bubble_sort(int *array, size_t size) {
-    int swapped;
-    for (size_t i = 0; i < size - 1; i++) {
-        swapped = 0;
-        for (size_t j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                // Swap array[j] and array[j + 1]
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                swapped = 1;
-
-                // Print the array after each swap
-                for (size_t k = 0; k < size; k++) {
-                    printf("%d ", array[k]);
-                }
-                printf("\n");
-            }
-        }
-        // If no swapping occurred in the inner loop, the array is already sorted
-        if (!swapped) {
-            break;
-        }
-    }
+int swapped;
+for (size_t i = 0; i < size - 1; i++) {
+swapped = 0;
+for (size_t j = 0; j < size - i - 1; j++) {
+if (array[j] > array[j + 1]) {
+int temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swapped = 1;
+for (size_t k = 0; k < size; k++) {
+printf("%d ", array[k]);
+}
+printf("\n");
+}
+}
+if (!swapped) {
+break;
+}
+}
 }
